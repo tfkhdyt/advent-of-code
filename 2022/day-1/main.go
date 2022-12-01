cecc126d72360b668532b999a5550a35468ce971
tfkhdyt <tfkhdyt@proton.me>
2022-12-01 16:01:34 +0700

    refactor(day 1): switch to for range

--- 2022/day-1/main.go
+++ 2022/day-1/main.go
@@ -51,12 +51,14 @@ func main() {
 		return elvesFoodsCalories[i] > elvesFoodsCalories[j]
 	})
 
+  elvesFoodsCalories = elvesFoodsCalories[:3]
+
 	fmt.Printf("Most calories: %v\n", elvesFoodsCalories[0])
 
 	var totalCaloriesOf3Elves uint64 = 0
-	for i := 0; i < 3; i++ {
-		totalCaloriesOf3Elves += elvesFoodsCalories[i]
-	}
+  for _, calories := range elvesFoodsCalories {
+    totalCaloriesOf3Elves += calories
+  }
 
-	fmt.Printf("Total calories carried by top 3 elves: %v", totalCaloriesOf3Elves)
+	fmt.Printf("Total calories carried by top 3 elves: %v\n", totalCaloriesOf3Elves)
 }
