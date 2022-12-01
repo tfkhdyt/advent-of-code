7d850cc620ac9e440326b41ef11e019f1a8540ec
tfkhdyt <tfkhdyt@proton.me>
2022-12-01 14:17:16 +0700

    feat(2022): add day 1

---
+++ 2022/day-1/go.mod
@@ -0,0 +1,3 @@
+module codeberg.org/tfkhdyt/advent-of-code/2022/day1
+
+go 1.19
---
+++ 2022/day-1/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	const fileName = "input.txt"
+	cwd, err := os.Getwd()
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	filePath := filepath.Join(cwd, fileName)
+
+	rawInput, err := os.ReadFile(filePath)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	elvesFoodsRawCalories := strings.Split(string(rawInput), "\n\n")
+
+	elvesFoodsCalories := []uint64{}
+
+	for _, elfFoodsRawCalories := range elvesFoodsRawCalories {
+		foodsCalories := strings.Split(elfFoodsRawCalories, "\n")
+		var totalOfElfFoodsCalories uint64 = 0
+
+		for _, foodCalories := range foodsCalories {
+			if foodCalories == "" {
+				continue
+			}
+			foodCaloriesUint, err := strconv.ParseUint(foodCalories, 10, 64)
+			if err != nil {
+				log.Panicln(err)
+			}
+
+			totalOfElfFoodsCalories += foodCaloriesUint
+		}
+
+		elvesFoodsCalories = append(elvesFoodsCalories, totalOfElfFoodsCalories)
+	}
+
+	sort.Slice(elvesFoodsCalories, func(i, j int) bool {
+		return elvesFoodsCalories[i] > elvesFoodsCalories[j]
+	})
+
+	fmt.Printf("Most calories: %v\n", elvesFoodsCalories[0])
+	// fmt.Printf("Most calories #2: %v\n", elvesFoodsCalories[1])
+	// fmt.Printf("Most calories #3: %v\n", elvesFoodsCalories[2])
+
+	var totalCaloriesOf3Elves uint64 = 0
+	for i := 0; i < 3; i++ {
+		totalCaloriesOf3Elves += elvesFoodsCalories[i]
+	}
+
+  fmt.Printf("Total calories carried by top 3 elves: %v", totalCaloriesOf3Elves) 
+}
+
